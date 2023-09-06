f146737e77e9648aedc2116a7d42adafcee7623b
YMVR <141694408+YMVR@users.noreply.github.com>
2023-09-06 00:13:52 -0400

    Add files via upload

---
+++ parte2.py
@@ -0,0 +1,9 @@
+import readchar
+
+while True:
+    print("presione la tecla ↑ para detenerse")
+    tecla_k = readchar.readkey()
+    if tecla_k -- readchar.key.UP:
+        break
+    print("Te detuviste")
+
